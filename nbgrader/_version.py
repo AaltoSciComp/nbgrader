48721670b24c02eee0813341f489b97d377e104d
Tunç Başar Köse <tuncbasark@gmail.com>
2023-01-17 13:38:07 +0200

    Change version number to 0.8.1.dev999 to indicate fork

--- nbgrader/_version.py
+++ nbgrader/_version.py
@@ -1,2 +1,2 @@
-version_info = (0, 8, 1, "", "")
-__version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
+version_info = (0, 8, 1, "dev999")
+__version__ = ".".join(map(str, version_info))
