2d1e9630b05173a146f0c1048dfe4ccb87d3e05e
Sami Laine <sami.v.laine@aalto.fi>
2023-01-24 17:20:00 +0200

    Version dev801

--- nbgrader/_version.py
+++ nbgrader/_version.py
@@ -1,2 +1,2 @@
-version_info = (0, 8, 1, "dev800")
+version_info = (0, 8, 1, "dev801")
 __version__ = ".".join(map(str, version_info))
--- src/validate_assignment/index.ts
+++ src/validate_assignment/index.ts
@@ -23,7 +23,7 @@ import { requestAPI } from './validateassignment';
 
 import { showNbGraderDialog, validate } from '../common/validate';
 
-var nbgrader_version = "0.8.1.dev800"; // TODO: hardcoded value
+var nbgrader_version = "0.8.1.dev801"; // TODO: hardcoded value
 
 const PLUGIN_ID = "nbgrader/validate-assignment"
 
