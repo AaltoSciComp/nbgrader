29b41b2be5964b2977f15b746cbea336839dae04
Sami Laine <sami.v.laine@aalto.fi>
2025-03-17 16:39:48 +0200

    Bump to 0.8.4.dev506

--- nbgrader/_version.py
+++ nbgrader/_version.py
@@ -1,2 +1,2 @@
-version_info = (0, 8, 4, ".dev", "505")
+version_info = (0, 8, 4, ".dev", "506")
 __version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
--- src/assignment_list/index.ts
+++ src/assignment_list/index.ts
@@ -150,7 +150,7 @@ class AssignmentListWidget extends Widget {
   checkNbGraderVersion() {
     var warning = this.node.getElementsByClassName('version_error')[0] as HTMLDivElement;
     warning.hidden=false;
-    requestAPI<any>('nbgrader_version?version='+"0.8.4.dev505")
+    requestAPI<any>('nbgrader_version?version='+"0.8.4.dev506")
     .then(response => {
         if (!response['success']) {
           warning.innerText = response['message'];
